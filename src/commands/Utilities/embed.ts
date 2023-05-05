130041313f85c582b22913d5a2dad3130373912e
Mieko Hikari <Mieko.Hikarii@gmail.com>
2023-05-05 14:36:32 +0400

    Finish introduction

--- src/commands/Utilities/embed.ts
+++ src/commands/Utilities/embed.ts
@@ -32,16 +32,25 @@ export class UserCommand extends Command {
 	}
 
 	private async handbook(interaction: Command.ChatInputCommandInteraction) {
-		const embed1 = new EmbedBuilder()
-			.setColor([43, 45, 49])
-			.setImage('https://raw.githubusercontent.com/MiekoHikari/VTA-Discord-Bot/major-development/src/assets/embed%20presets/handbook/Cover.png');
-
+		const embed1 = this.embedBanner("https://raw.githubusercontent.com/MiekoHikari/VTA-Discord-Bot/major-development/src/assets/embed%20presets/handbook/Cover.png");
 		const embed2 = new EmbedBuilder()
 			.setColor([233, 77, 81])
-			.setAuthor({ name: '#Author' })
+			.setAuthor({ name: '#Introduction' })
+			.setTitle('Welcome to the VTuber Academy!')
+			.setDescription(
+				' I am a cat to not be touched but guide you to feel at ease in our humble server! This handbook is designed to guide you around our server and teach you the principles we follow to maintain peace and justice! This handbook is available all the time by using /handbook in chat!'
+			);
 
 		interaction.channel?.send({
 			embeds: [embed1, embed2],
 		})
+	}
+
+	private embedBanner(url: string) {
+		const embed = new EmbedBuilder()
+			.setColor([43, 45, 49])
+			.setImage(url);
+
+		return embed;
 	}
 }
